7542d2b953e860ef8e48575ae317cb82a2d17670
Justin Crause <juzz@crause.com>
2024-07-20 20:14:27 +0200

    Bumping version and fixing console output for name

--- src/stack-in-card.ts
+++ src/stack-in-card.ts
@@ -1,10 +1,5 @@
-import { CARD_NAME } from "./card/const";
-import * as pjson from "../package.json";
+import { version } from "../package.json";
 import "./card/stack-in-card";
 
 /* eslint no-console: 0 */
-console.info(
-  `%c  ${CARD_NAME.toUpperCase}  \n%c Version ${pjson.version} `,
-  "color: orange; font-weight: bold; background: black",
-  "color: white; font-weight: bold; background: dimgray",
-);
+console.info(`%c📐 Stack-In-Card 📐 - ${version} `, "color: #d597ff; font-weight: 700;");
